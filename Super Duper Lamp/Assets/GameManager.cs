9b60b1e468429929fd1045ec0ce1f9335e69129d
Eliot Laidlaw <laidlawe18@barringtonschools.org>
2018-04-01 08:51:17 -0400

    Added GameManager script

--- Super Duper Lamp/Assets/GameManager.cs
+++ Super Duper Lamp/Assets/GameManager.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    // A publicly accessible variable that contains the one instance of GameManager (the script)
+    // This can be accessed anywhere by doing GameManager.instance
+    public static GameManager instance;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Set instance to be this newly created object when it starts
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+
 	}
 	
 	// Update is called once per frame
